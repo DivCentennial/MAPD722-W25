51fccce42af9e75f80d2a488a4a00ca392be1395
rania <raniaarbash@hotmail.com>
2025-01-15 10:13:00 -0500

    DiceApp-Week2

--- week2/lib/main.dart
+++ week2/lib/main.dart
@@ -1,10 +1,26 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(DieRollApp());
 }
 
-class DieRollApp extends StatelessWidget {
+class DieRollApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _DieRollState();
+  }
+}
+
+class _DieRollState extends State<DieRollApp> {
+  var randomInt = Random().nextInt(6) + 1;
+  void rollDie() {
+    setState(() {
+      randomInt = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,6 +30,7 @@ class DieRollApp extends StatelessWidget {
         ),
         body: Center(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
               'Roll a Die',
@@ -23,7 +40,11 @@ class DieRollApp extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            Image.asset('', width: 150, height: 159)
+            Image.asset('assets/dice-$randomInt.png', width: 150, height: 150),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(onPressed: rollDie, child: const Text('Roll'))
           ],
         )),
       ),
